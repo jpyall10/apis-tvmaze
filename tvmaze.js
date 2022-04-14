1b4e354a509bd649c7a6cfdf68329dea9b8ca1fc
Jon Porter <jonp@jons-air.lan>
2022-04-14 19:11:24 -0400

    ready to submit, changed so shows list clears on episodes click

--- tvmaze.js
+++ tvmaze.js
@@ -2,6 +2,7 @@
 
 const $showsList = $("#shows-list");
 const $episodesArea = $("#episodes-area");
+const $episodesList = $("#episodes-list");
 const $searchForm = $("#search-form");
 const BASE_API_URL = "http://api.tvmaze.com/";
 const MISSING_IMAGE_URL = " https://tinyurl.com/tv-missing";
@@ -14,8 +15,6 @@ const MISSING_IMAGE_URL = " https://tinyurl.com/tv-missing";
  */
  async function getShowsByTerm(searchTerm) {
   const response = await axios.get(`${BASE_API_URL}search/shows?q=${searchTerm}`);
-  console.log(response);
-
   return response.data.map(result => {
     const show = result.show;
     return {
@@ -34,7 +33,7 @@ function populateShows(shows) {
 
   for (let show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+        `<div data-show-id="${show.id}" data-show-name="${show.name}" data-show-summary="${show.summary}" data-show-image-src="${show.image}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img 
               src=${show.image} 
@@ -53,6 +52,46 @@ function populateShows(shows) {
 
     $showsList.append($show);  }
 }
+
+function populateShow(show) {
+  $showsList.empty();
+  console.log(show);
+
+  const $show = $(
+    `<div data-show-id="${show.id}" data-show-name="${show.name}" data-show-summary="${show.summary}" data-show-image-src="${show.image}" class="Show col-md-12 col-lg-6 mb-4">
+     <div class="media">
+       <img 
+          src=${show.image} 
+          alt=${MISSING_IMAGE_URL} 
+          class="w-25 mr-3">
+       <div class="media-body">
+         <h5 class="text-primary">${show.name}</h5>
+         <div><small>${show.summary}</small></div>
+         <button class="btn btn-outline-light btn-sm Show-getEpisodes">
+           Episodes
+         </button>
+       </div>
+     </div>  
+   </div>
+  `);
+
+$showsList.append($show);
+}
+
+// async function getShowBy(id){
+//   $showsList.empty();
+//   const response = await axios.get(`${BASE_API_URL}/shows/${id})`);
+//   console.log(results.data);
+//   return response.data.map(result => {
+//     const show = result.show;
+//     return {
+//       id: show.id,
+//       name: show.name,
+//       summary: show.summary,
+//       image: show.image ? show.image.medium : MISSING_IMAGE_URL,
+//     };
+//   });
+// }
 
 
 /** Handle search form submission: get shows from API and display.
@@ -73,13 +112,46 @@ $searchForm.on("submit", async function (evt) {
   await searchForShowAndDisplay();
 });
 
-
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function getEpisodesOfShow(id) { 
+  const response = await axios.get(`${BASE_API_URL}shows/${id}/episodes`);
+  return response.data.map(episode =>({
+    id: episode.id,
+    name: episode.name,
+    season: episode.season,
+    number: episode.number,
+  }));
+
+}
 
 /** Write a clear docstring for this function... */
 
-// function populateEpisodes(episodes) { }
+function populateEpisodes(episodes) { 
+  $episodesList.empty();
+  for (let episode of episodes) {
+    const $e = $(`<li>${episode.name} (Season ${episode.season}, Number ${episode.number})</li>`);
+    $episodesList.append($e);  
+  }
+  // $showsList.empty();
+
+  $episodesArea.show();
+}
+
+async function searchForEpisodesAndDisplay(event) {
+  
+  const id = $(event.target).closest(".Show").data("show-id");
+  const name = $(event.target).closest(".Show").data("show-name");
+  const image = $(event.target).closest(".Show").data("show-image-src");
+  const summary = $(event.target).closest(".Show").data("show-summary");
+  const show = {id,name,image,summary,};
+
+  const episodes = await getEpisodesOfShow(id);
+  populateShow(show);
+  populateEpisodes(episodes);
+}
+
+$showsList.on("click",".Show-getEpisodes",searchForEpisodesAndDisplay);
+
